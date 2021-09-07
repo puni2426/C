c7c2d69a5bdd2b635dd3e0e05de3bb5944bc346d
Punith S <53472959+puni2426@users.noreply.github.com>
2021-09-07 19:55:52 +0530

    Pattern Printing

---
+++ pgm 1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    int n,i,j;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+      
+        for(j=1;j<=i;j++)
+        {
+            if(i%2!=0)
+            {
+                printf("%d ",(2*j)-1);
+               
+            }
+            else
+            {
+                printf("%d ",2*j);
+             
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
